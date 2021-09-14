f113f92d34fc4f9cc4be7e693828af48fc1e6264
build-a-website <90342330+build-a-website@users.noreply.github.com>
2021-09-13 23:43:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,29 +5,31 @@ const port = process.env.PORT || 3000;
 
 const Hapi = require('@hapi/hapi');
 
-const init = async () => {
-
-    const server = Hapi.server({
-        port: port,
-        host: hostname
-    });
-  
-    server.route({
-        method: 'GET',
-        path: '/{any*}',
-        handler: (request, h) => {
-
-            return 'Hola Mundo!';
+const server = Hapi.server({
+    port: port,
+    host: hostname,
+    routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'static')
+            }
         }
-    });
+});
 
-    await server.start();
+server.route({
+    method: 'GET',
+    path: '/{any*}',
+    handler: {
+        directory: {
+            path: 'static'
+        }
+    }
+});
+
+server.start().then( server => {
     console.log('Server running on %s', server.info.uri);
-};
+})
 
 process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
 });
-
-init();
